49595c5b4553e6ddbf29c7993c33e80983c7cc51
Dinis Cruz <dinis.cruz@owasp.org>
2015-04-26 13:30:07 +0100

    testing travis encrypted git clone command

--- bin/before.sh
+++ bin/before.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-echo '[before] this is in the before script'
+#if [ ! -f ./Lib_UNO/UNO.xml ]; then
+#  echo 'Cloning Lib_UNO repository'
+#  git clone git@github.com:TMContent/Lib_UNO.git
+#else
+#  echo 'Updating Lib_UNO repository'
+#  git pull origin master
+#fi
