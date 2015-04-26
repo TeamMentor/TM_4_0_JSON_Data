a3ae917fc48e9fb5133de155c0197bf75cba77d0
Dinis Cruz <dinis.cruz@owasp.org>
2015-04-26 15:57:05 +0100

    Improving .travis.yml file

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,11 @@
 if [ ! -f ./Lib_UNO/UNO.xml ]; then
   echo 'Cloning Lib_UNO repository'
   git clone git@github.com:TMContent/Lib_UNO.git
+  git clone git@github.com:tm-build/Lib_UNO-json.git
 else
   echo 'Updating Lib_UNO repository'
   git pull origin master
+  cd Lib_UNO-json
+  git pull origin master
+  cd ..
 fi
